e2001a14aa883727568126894f8abae8292eee8f
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-08-06 17:02:38 +0300

    CRUD process is completed. Images are added

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,33 @@ import './App.css'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  "YOUR_SUPABASE_API Settings"
-  ,"YOUR_SUPABASE_PROJECT_API_KEYS"
-  );
+  // "YOUR_SUPABASE_API Settings"
+  // , "YOUR_SUPABASE_PROJECT_API_KEYS"
+);
 
 function App() {
 
   const [todos, setTodos] = useState([])
   const [text, setText] = useState("")
 
+
+
   async function createTodo() {
     await supabase
       .from('todos')
       .insert({ title: text })
+    setText("")
     await getTodos()
   }
+
+  async function getTodos() {
+    const { data } = await supabase
+      .from("todos")
+      .select()
+      .order("id")
+    setTodos(data)
+  }
+
   async function updateTodoDone(todo) {
     await supabase
       .from("todos")
@@ -25,33 +37,48 @@ function App() {
       .eq("id", todo.id);
     await getTodos();
   }
-  async function getTodos() {
-    const { data } = await supabase
-    .from("todos")
-    .select()
-    .order("id")
-    setTodos(data)
+
+  async function deleteTodo(todo) {
+    await supabase
+      .from('todos')
+      .delete()
+      .eq('id', todo.id);
+
+    await getTodos()
   }
+
   useEffect(() => {
     getTodos()
   }, [])
 
   return (
     <>
-      <input
-        value={text}
-        placeholder='Type Task Title'
-        onChange={(e) => setText(e.target.value)} />
-      <button onClick={createTodo}>Add</button>
-      {todos.map((todo) => (
-        <div key={todo.id}>
+
+      <div className="todo-container">
+        <h1>Todo List</h1>
+        <div>
           <input
-            type="checkbox"
-            checked={todo.done}
-            onChange={(e) => updateTodoDone(todo)} />
-          {todo.title}
+            value={text}
+            placeholder='Type Task Title'
+            onChange={(e) => setText(e.target.value)} />
+          <button onClick={createTodo}>Add</button>
         </div>
-      ))}
+        <ul>
+
+          {todos.map((todo) => (
+            <li key={todo.id} className={todo.done ? 'completed' : ''}>
+              <input
+                type="checkbox"
+                checked={todo.done}
+                onChange={() => updateTodoDone(todo)} />
+              {<span>{todo.title}</span>}
+              <button onClick={() => deleteTodo(todo)}>X</button>
+              {/* </div> */}
+            </li>)
+          )}
+        </ul>
+      </div>
+
     </>
   )
 }
